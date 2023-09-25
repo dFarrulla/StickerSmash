1b6c5cdac979464adac8f367c5fb4a08a7905daf
Daniel Farrulla <104379290+dFarrulla@users.noreply.github.com>
2023-09-25 13:12:00 -0300

    Primeira postagem no StickerSmash
    Adicionamos a funcionalidade de primeira postagem ao projeto StickerSmash. Isso permite que os usuários escolham ou forneçam uma imagem, adicionar emojis a ela, movê-los e redimensioná-los. Além disso, os usuários agora podem salvar a imagem resultante na galeria do telefone.

---
+++ StickerSmash/App.js
@@ -0,0 +1,175 @@
+import { StatusBar } from "expo-status-bar";
+import { useState, useRef } from "react";
+import { StyleSheet, View, Platform  } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
+import * as MediaLibrary from 'expo-media-library';
+
+// Components
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+import IconButton from './components/IconButton';
+import CircleButton from './components/CircleButton';
+import EmojiSticker from './components/EmojiSticker';
+import EmojiPicker from './components/EmojiPicker';
+import EmojiList from './components/EmojiList'
+
+const PlaceholderImage = require("./assets/images/background-image.png");
+
+export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+
+
+  const imageRef = useRef();
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+      setIsModalVisible(true)
+    } else {
+      alert('Você não selecionou nenhuma imagem.');
+    }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+    setPickedEmoji(null);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      domtoimage
+        .toJpeg(imageRef.current, {
+            quality: 0.95,
+            width: 320,
+            height: 440,
+          })
+        .then(dataUrl => {
+          let link = document.createElement('a');
+          link.download = 'sticker-smash.jpeg';
+          link.href = dataUrl;
+          link.click();
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  };
+  
+
+  // Função para solicitar permissão e, em seguida, salvar o arquivo
+const saveFileToLibrary = async (localUri) => {
+  // Solicitar permissão de escrita à biblioteca de mídia
+  const { status } = await MediaLibrary.requestPermissionsAsync();
+
+  if (status === 'granted') {
+    // A permissão foi concedida, agora você pode salvar o arquivo
+    try {
+      const asset = await MediaLibrary.createAssetAsync(localUri);
+      await MediaLibrary.createAlbumAsync('Nome do seu álbum', asset, false);
+      console.log('Arquivo salvo com sucesso na biblioteca de mídia.');
+    } catch (error) {
+      console.error('Erro ao salvar o arquivo na biblioteca de mídia:', error);
+    }
+  } else {
+    console.log('Permissão de escrita à biblioteca de mídia não concedida.');
+  }
+};
+
+// Chamada da função para salvar um arquivo (substitua 'localUri' pelo seu URI)
+saveFileToLibrary('localUriDoSeuArquivo');
+  
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer
+            placeholderImageSource={PlaceholderImage}
+            selectedImage={selectedImage}
+          />
+          {pickedEmoji !== null ? (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          ) : null}
+        </View>
+      </View>
+
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reiniciar" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Salvar" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Escolha uma foto" onPress={pickImageAsync} />
+          <Button label="Use esta foto" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
+
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
+      <StatusBar style="auto" />
+      {/* <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} /> */}
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+});
---
+++ StickerSmash/README.md
@@ -0,0 +1,8 @@
+# StickerSmash
+The Expo Tutorial for adding Emojis to Pictures in Expo
+
+[Static Web App](https://jongan69.github.io/StickerSmash/)
+
+[Mobile App](https://expo.dev/@jongan69/StickerSmash)
+
+[Tutorial](https://docs.expo.dev/tutorial/introduction/)
